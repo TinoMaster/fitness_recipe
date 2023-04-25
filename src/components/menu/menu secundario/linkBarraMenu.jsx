679dafe5ca2da512556de722129107293408fcb7
TinoMaster <oscarmarcos2673@gmail.com>
2023-04-25 19:20:38 -0400

    added framer motion to the project

--- src/components/menu/menu secundario/linkBarraMenu.jsx
+++ src/components/menu/menu secundario/linkBarraMenu.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
+import { motion } from "framer-motion";
 
 export const LinkBarraMenu = () => {
   return (
-    <div className="flex w-full md:w-1/2">
-      <ul className="flex w-full justify-around md:justify-start items-center">
+    <div className="flex w-full">
+      <motion.ul
+        initial={{ opacity: 0, x: "-100vw" }}
+        animate={{ opacity: 1, x: 0 }}
+        /* transition={{ duration: 0.5 }} */
+        className="flex w-full justify-center items-center"
+      >
         <Link className="link-menu" to={"/"}>
           <AiFillHome size={"28px"} />
         </Link>
@@ -18,7 +24,7 @@ export const LinkBarraMenu = () => {
         <Link className="link-menu" to={"/recipe"}>
           Recetas
         </Link>
-      </ul>
+      </motion.ul>
     </div>
   );
 };
